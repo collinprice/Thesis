8a14ed0c11c0e9335c05623afd44df3ec5c9559f
Collin Price <collin.price@gmail.com>
2014-02-26 14:41:49 -0500

    added title and abstract

---
+++ thesis/compile-latex.sh
@@ -0,0 +1,2 @@
+latex thesis.tex && pdflatex thesis.tex && gnome-open thesis.pdf
+#rm -f thesis.bbl && latex thesis.tex && bibtex thesis && latex thesis.tex && pdflatex thesis.tex && gnome-open thesis.pdf
