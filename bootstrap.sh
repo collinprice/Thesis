67380724dbdc5f4771d15ad21443935bf6aa6fef
Collin Price <collin@furi.ca>
2014-04-20 22:22:53 -0400

    intro and previous work drafts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 sudo apt-get -y update
-sudo apt-get -y install vim texlive texlive-publishers texlive-science texlive-latex-extra
+sudo apt-get -y install vim texlive texlive-publishers texlive-science texlive-latex-extra aspell
 
