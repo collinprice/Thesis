1a546f68534c887276115a66aa5177f7769b7b55
Collin Price <collin@furi.ca>
2014-04-20 20:27:20 -0400

    initial writing

--- thesis/compile-latex.sh
+++ thesis/compile-latex.sh
@@ -1,2 +1,4 @@
-latex thesis.tex && pdflatex thesis.tex && gnome-open thesis.pdf
-#rm -f thesis.bbl && latex thesis.tex && bibtex thesis && latex thesis.tex && pdflatex thesis.tex && gnome-open thesis.pdf
+latex thesis && bibtex thesis && latex thesis && pdflatex thesis
+
+
+# latex thesis.tex && pdflatex thesis.tex && gnome-open thesis.pdf
