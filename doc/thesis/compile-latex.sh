a2e21edfecc4cd6264a22815ef4a3a056a4da0dd
Collin Price <collin@collinprice.com>
2014-08-06 18:01:00 -0400

    finished de

--- doc/thesis/compile-latex.sh
+++ doc/thesis/compile-latex.sh
@@ -1,4 +1,4 @@
 # rm -f thesis.bbl && latex thesis && bibtex thesis && latex thesis && pdflatex thesis && gnome-open thesis.pdf
+latex thesis && bibtex thesis && latex thesis && pdflatex thesis && gnome-open thesis.pdf
 
-
-latex thesis.tex && pdflatex thesis.tex && gnome-open thesis.pdf
+# latex thesis.tex && pdflatex thesis.tex && gnome-open thesis.pdf
