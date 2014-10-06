ddf7f9d48ba08298893331a719d1e1b8ede2a4b7
Collin Price <collin@collinprice.com>
2014-10-06 11:40:53 -0400

    ready

--- doc/thesis-presentation/compile-latex.sh
+++ doc/thesis-presentation/compile-latex.sh
@@ -1,2 +1,4 @@
-latex presentation.tex && pdflatex presentation.tex && gnome-open presentation.pdf
+# latex presentation.tex && pdflatex presentation.tex && gnome-open presentation.pdf
+
+latex presentation && bibtex presentation && latex presentation && pdflatex presentation && xdg-open presentation.pdf
 #rm -f presentation.bbl && latex presentation.tex && bibtex presentation && latex presentation.tex && pdflatex presentation.tex && gnome-open presentation.pdf
